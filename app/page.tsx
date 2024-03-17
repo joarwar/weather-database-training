8955fe06bbdea87bb976cb49ac4305276046c6f9
joarwar <91726192+joarwar@users.noreply.github.com>
2024-03-17 19:51:50 +0100

    First commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,76 @@
-import Image from "next/image";
+"use client";
+// pages/index.tsx
+import { useState } from "react";
 
-export default function Home() {
+async function getData(city: string) {
+  const res = await fetch(
+    `http://api.weatherapi.com/v1/forecast.json?key=0a9b864a33dc410d9a5173252241703&q=${city}&days=2&aqi=no&alerts=no`
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default function Page() {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState<any>(null); // Assuming any type for weatherData
+  const fetchWeather = async () => {
+    try {
+      const data = await getData(city);
+      setWeatherData(data);
+    } catch (error) {
+      console.error("Error fetching weather:", error);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+<main className="min-h-screen bg-gray-100 py-8 px-4">
+  <div className="max-w-md mx-auto bg-white p-6 rounded-lg shadow-md">
+    <h1 className="text-3xl font-semibold mb-4">Search a city and check the weather</h1>
+    <div className="flex items-center mb-4">
+      <input
+        type="text"
+        placeholder="Enter city"
+        className="border border-gray-300 rounded-l-md px-4 py-2 w-full"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+      <button className="bg-slate-400 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-r-md" onClick={fetchWeather}>Check</button>
+    </div>
+    {weatherData && (
+      <div>
+        <h2 className="text-xl font-semibold mb-4">Current Weather in {weatherData.location.name}, {weatherData.location.country}</h2>
+        <div className="bg-gray-200 p-4 rounded-md mb-4 flex items-center">
+          <img src={`http:${weatherData.current.condition.icon}`} alt={weatherData.current.condition.text} className="w-12 h-12 mr-4" />
+          <div>
+
+            <p className="text-lg">Temperature: {weatherData.current.temp_c}°C {weatherData.current.temp_c > 7 ? '😎' : weatherData.current.temp_c < 7 && '🥶'}</p>
+            <p className="text-lg">Condition: {weatherData.current.condition.text}</p>
+          </div>
+        </div>
+        <div>
+          <h2 className="text-xl font-semibold mb-2">Tomorrow's Weather Forecast</h2>
+          {weatherData.forecast && weatherData.forecast.forecastday.length > 1 && (
+            <div className="bg-gray-200 p-4 rounded-md">
+              <h3 className="text-lg font-semibold mb-2">{weatherData.forecast.forecastday[1].date}</h3>
+              <div className="flex items-center">
+                <img src={`http:${weatherData.forecast.forecastday[1].day.condition.icon}`} alt={weatherData.forecast.forecastday[1].day.condition.text} className="w-10 h-10 mr-4" />
+                <div>
+                  <p className="text-lg">Max Temperature: {weatherData.forecast.forecastday[1].day.maxtemp_c}°C {weatherData.forecast.forecastday[1].day.maxtemp_c > 7 ? '😎' : weatherData.forecast.forecastday[1].day.maxtemp_c < 7 && '🥶'}</p>
+                  <p className="text-lg">Min Temperature: {weatherData.forecast.forecastday[1].day.mintemp_c}°C {weatherData.forecast.forecastday[1].day.mintemp_c > 7 ? '😎' : weatherData.forecast.forecastday[1].day.mintemp_c < 7 && '🥶'}</p>
+                  <p className="text-lg">Weather: {weatherData.forecast.forecastday[1].day.condition.text}</p>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
+    )}
+  </div>
+</main>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   );
 }
